3c3dd3301dc4c22bcfcccac12712acc58135b513
Yuri Stanislavsky <stanislavskiy@me.com>
2021-01-04 18:14:49 +0200

    Initial Commit

---
+++ Slider-SwiftUI/Slider_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slider_SwiftUIApp.swift
+//  Slider-SwiftUI
+//
+//  Created by Юрий Станиславский on 04.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct Slider_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
